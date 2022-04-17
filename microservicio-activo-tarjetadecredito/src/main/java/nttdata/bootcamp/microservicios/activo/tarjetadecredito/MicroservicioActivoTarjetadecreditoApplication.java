30b5606b62e37e5e8b0c5875096ec2c7a236e2c8
Joffre Hermosilla Salas <jhermoss@emeal.nttdata.com>
2022-04-17 11:35:08 -0500

    primer commit

---
+++ microservicio-activo-tarjetadecredito/src/main/java/nttdata/bootcamp/microservicios/activo/tarjetadecredito/MicroservicioActivoTarjetadecreditoApplication.java
@@ -0,0 +1,13 @@
+package nttdata.bootcamp.microservicios.activo.tarjetadecredito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioActivoTarjetadecreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioActivoTarjetadecreditoApplication.class, args);
+	}
+
+}
---
+++ microservicio-activo-tarjetadecredito/src/test/java/nttdata/bootcamp/microservicios/activo/tarjetadecredito/MicroservicioActivoTarjetadecreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package nttdata.bootcamp.microservicios.activo.tarjetadecredito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioActivoTarjetadecreditoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
